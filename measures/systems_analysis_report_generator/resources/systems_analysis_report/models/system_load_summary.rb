a0ba63fc58567d3303ad86bdedf0e65503be9a35
pflaumingo <pflaumingo@users.noreply.github.com>
2020-07-06 16:50:50 +0000

    Automatic merge from the SystemsAnalysisReport repo

--- measures/systems_analysis_report_generator/resources/systems_analysis_report/models/system_load_summary.rb
+++
@@ -1,9 +0,0 @@
-module SystemsAnalysisReport
-  module Models
-    SystemLoadSummary = Struct.new(:name, :peak_condition, :engineering_check, :estimated_peak_load_component_table,
-                                   :temperature, :airflow) do
-
-      include Models::Model
-    end
-  end
-end
