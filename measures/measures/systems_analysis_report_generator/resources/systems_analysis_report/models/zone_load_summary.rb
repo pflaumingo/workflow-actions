20fa622a533c97e073b46715295e12b18946d8fa
pflaumingo <pflaumingo@users.noreply.github.com>
2020-07-06 16:55:49 +0000

    Automatic merge from the SystemsAnalysisReport repo

--- measures/measures/systems_analysis_report_generator/resources/systems_analysis_report/models/zone_load_summary.rb
+++
@@ -1,8 +0,0 @@
-module SystemsAnalysisReport
-  module Models
-    ZoneLoadSummary = Struct.new(:peak_condition, :engineering_check, :estimated_peak_load_component_table) do
-      include Models::Model
-
-    end
-  end
-end
